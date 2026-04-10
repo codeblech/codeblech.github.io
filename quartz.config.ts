739db0a3fe40bf4f2b61b6643f0628131474d7a0
codeblech <my.malikyash@gmail.com>
2026-04-10 19:17:29 +0530

    Quartz sync: Apr 10, 2026, 7:17 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Rokkitt",
+        header: "Bangers",
         body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
