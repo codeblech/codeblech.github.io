f18a2d9a8eccefdfafca064ab55b49db7b13f012
codeblech <my.malikyash@gmail.com>
2026-08-07 00:23:13 +0530

    raven

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -5,11 +5,22 @@ const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
   // If baseUrl contains a pathname after the domain, use this as the home link
   const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
   const baseDir = url.pathname
+  const staticBaseDir = baseDir.replace(/\/$/, "")
 
   return (
     <article class="popover-hint">
-      <h1>404</h1>
-      <p>{i18n(cfg.locale).pages.error.notFound}</p>
+      <img src={`${staticBaseDir}/Attachments/the-raven.png`} alt="The Raven" class="not-found-img" />
+      <pre class="not-found-poem">{`Once upon a midnight dreary, while I websurfed, weak and weary,
+
+Over many a strange and spurious website of 'hot chicks galore',
+
+While I clicked my fav'rite bookmark, suddenly there came a warning,
+
+And my heart was filled with mourning, mourning for my dear amour.
+
+"'Tis not possible," I muttered, "give me back my cheap hardcore!" -
+
+Quoth the server, "404".`}</pre>
       <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
     </article>
   )
